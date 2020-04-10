8df53970fbd12b9aa8ec3717457e058590773c47
Ross Baker <rossbaker32@gmail.com>
2020-04-10 10:38:52 +1000

    setup ruby on rails with rask scaffolding

---
+++ flattenTheCurve/db/migrate/20200410003754_create_tasks.rb
@@ -0,0 +1,15 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.datetime :start_time
+      t.datetime :end_time
+      t.string :description
+      t.integer :priority
+      t.boolean :completed
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
