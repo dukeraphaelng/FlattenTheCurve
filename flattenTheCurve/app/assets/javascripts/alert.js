e99aba6c409c069b5c4cca4b27f2216a81e1a80e
Ross Baker <rossbaker32@gmail.com>
2020-04-10 15:34:18 +1000

    added a popup alert for task end

--- flattenTheCurve/app/assets/javascripts/alert.js
+++ flattenTheCurve/app/assets/javascripts/alert.js
@@ -4,9 +4,11 @@ setInterval(() => {
   for (let item of endTimes) {
 
     endDate = new Date(item.innerHTML)
-
-    if (endDate <= new Date()) {
-      alert("task finished");
+    console.log(item.dataset.complete);
+    
+    if (endDate <= new Date() && item.dataset.complete == "no") {
+      alert(item.dataset.title + " - Time is up on this task");
+      item.dataset.complete = "yes"
     }
   }
 }, 1000);
--- flattenTheCurve/app/assets/javascripts/application.js
+++ flattenTheCurve/app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require alert
 
-require("alert.js")
+// require("alert.js")
 
