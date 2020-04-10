066aebe12e7ba7e0cbfab48901b46d175d59cf96
Morgan <morgan.batterham@gmail.com>
2020-04-10 19:40:35 +1000

    routing fix

--- flattenTheCurve/config/routes.rb
+++ flattenTheCurve/config/routes.rb
@@ -2,9 +2,5 @@ Rails.application.routes.draw do
   resources :tasks
 
   root to: "public#home"
-
-  get "tasks/login" to "tasks#login"
-
-  get "tasks/register" to "tasks#register"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
